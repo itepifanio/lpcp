753868c926071ce56940ac65f90914e72f37d8ba
Italo Epifânio de Lima e Silva <itepifanio@A307-17L.imd.local>
2020-03-11 14:30:12 -0300

    Inicia exercicio 5 da lista 1

--- exercicio1.hs
+++ exercicio1.hs
@@ -1,9 +1,36 @@
 module Exercicio1 where
 {- ghci exercicio1.hs vendas.hs -}
 import qualified Vendas
+import Prelude hiding (maximum)
+import Data.Char (toUpper, isLower)
 
+{- 1. -}
 semZeroNoPeriodo :: Int -> Bool
 semZeroNoPeriodo n = 
     all (\x -> Vendas.vendas x /= 0) [0..n]
 
-funct x y z = y < x
+{- 2. -}
+funct :: Int -> Int -> Int -> Bool
+funct x y z = not ((not (x > z)) && (y >= x))
+
+{- 3 -}
+yellIfYouTalkToMe :: Char -> Char
+yellIfYouTalkToMe c
+    | isLower c = toUpper(c)
+    | otherwise = c
+
+{- 4 -}
+bin x y = x - 2*y
+
+{- 5 -}
+maximum [x]    = x
+maximum (x:xs)
+    | x > maximum xs = x
+    | otherwise      = maximum xs
+
+maxSellDays :: Int -> List
+maxSellDays day
+    
+    where vendas = map Vendas.vendas [0..day]
+
+
